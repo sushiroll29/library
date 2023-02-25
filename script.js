e95e7006e2389503f21e9eb60ed76333f7c7dc06
Ale <ale.surdu29@gmail.com>
2023-02-25 23:37:24 +0100

    Create book cards dinamically

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const h = document.querySelector('.a');
 const libraryDiv = document.querySelector('.library');
 
 let library = [];
@@ -7,7 +6,7 @@ function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this. read = read;
+    this.read = read;
 }
 
 Book.prototype.info = function(){
@@ -15,7 +14,9 @@ Book.prototype.info = function(){
 }
 
 const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 255, 'not read yet');
-const book2 = new Book('Harry Potter and the Goblet of Fire', 'J.K. Roling', 355, 'read');
+const book2 = new Book('Harry Potter and the Goblet of Fire', 'J.K.Rowling', 355, 'read');
+const book3 = new Book('The Ballad of Songbirds and Snakes', 'Suzanne Collins', 355, 'read');
+
 
 function addBookToLibrary(book){
     library.push(book);
@@ -24,12 +25,33 @@ function addBookToLibrary(book){
 function displayLibrary(){
     library.forEach(book => {
         addBookToLibrary(book);
-        let p = document.createElement('p');
-        p.textContent = book.info();
-        libraryDiv.appendChild(p);
+
+        const bookCard = document.createElement('div');
+        bookCard.className = 'book';
+        const title = document.createElement('p');
+        title.className = 'title';
+        const author = document.createElement('p');
+        author.className = 'author';
+        const info = document.createElement('div');
+        info.className = 'info';
+        const pageNumber = document.createElement('span');
+        const read = document.createElement('span');
+
+        libraryDiv.appendChild(bookCard);
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(info);
+        info.appendChild(pageNumber);
+        info.appendChild(read);
+
+        title.textContent = book.title;
+        author.textContent = 'by ' + book.author;
+        pageNumber.textContent = book.pages + ' pages';
+        read.textContent = book.read;
     })
 }
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+addBookToLibrary(book3);
 displayLibrary();
