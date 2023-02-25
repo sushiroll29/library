036f2325621a5ff5cb38a45f349141613040cf29
Ale <ale.surdu29@gmail.com>
2023-02-25 23:15:30 +0100

    Add basic card layout

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const h = document.querySelector('.a');
-console.log(h.textContent);
+const libraryDiv = document.querySelector('.library');
+
+let library = [];
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -12,10 +14,22 @@ Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.pages} pages long, ${this.read}.`;
 }
 
-const book = new Book('The Hobbit', 'J.R.R. Tolkien', 255, 'not read yet');
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 255, 'not read yet');
+const book2 = new Book('Harry Potter and the Goblet of Fire', 'J.K. Roling', 355, 'read');
 
-function addBook(){
-    h.textContent = book.info();
+function addBookToLibrary(book){
+    library.push(book);
 }
 
-addBook();
+function displayLibrary(){
+    library.forEach(book => {
+        addBookToLibrary(book);
+        let p = document.createElement('p');
+        p.textContent = book.info();
+        libraryDiv.appendChild(p);
+    })
+}
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+displayLibrary();
